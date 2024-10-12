513a21cd4f0be64bddccc534188bfb366ff9e4e3
Emelme <125494917+Emelme@users.noreply.github.com>
2024-10-12 13:26:57 +0200

    Added DeliveryResultUI

---
+++ KitchenChaosLearning/Assets/MainFolder/Scripts/UI/DeliveryResultUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DeliveryResultUI : MonoBehaviour
+{
+	[SerializeField] private Image backgroundImage;
+	[SerializeField] private Color successColor;
+	[SerializeField] private Color failedColor;
+	[SerializeField] private TextMeshProUGUI messageText;
+	[SerializeField] private Animator animator;
+	private const string POPUP_TRIGGER = "popupTrigger";
+
+	private void Start()
+	{
+		DeliveryManager.Instance.OnDeliverySuccess += DeliveryManager_OnDeliverySuccess;
+		DeliveryManager.Instance.OnDeliveryFailed += DeliveryManager_OnDeliveryFailed;
+
+		gameObject.SetActive(false);
+	}
+
+	private void DeliveryManager_OnDeliverySuccess(Vector3 obj)
+	{
+		backgroundImage.color = successColor;
+		messageText.text = "DELIVERY\nSUCCESS";
+		animator.SetTrigger(POPUP_TRIGGER);
+
+		gameObject.SetActive(true);
+	}
+
+	private void DeliveryManager_OnDeliveryFailed(Vector3 obj)
+	{
+		backgroundImage.color= failedColor;
+		messageText.text = "DELIVERY\nFAILED";
+		animator.SetTrigger(POPUP_TRIGGER);
+
+		gameObject.SetActive(true);
+	}
+}
